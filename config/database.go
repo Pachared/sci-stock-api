91505e662c98b3945278aecdc7cf38e3d6efbb94
Axm.pachara <pachared.amr12@gmail.com>
2026-01-29 00:19:18 +0700

    เปลี่ยนตัวเชื่อมข้อมูล Getenv เป็นของ Railway

--- config/database.go
+++ config/database.go
@@ -21,11 +21,11 @@ func Connect() {
 	}
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("MYSQLUSER"),
+		os.Getenv("MYSQLPASSWORD"),
+		os.Getenv("MYSQLHOST"),
+		os.Getenv("MYSQLPORT"),
+		os.Getenv("MYSQLDATABASE"),
 	)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
