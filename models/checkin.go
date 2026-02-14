c526111d2d7a69a6c61c9b3a1e41e3b21be65d50
pachara <pachared.amr12@gmail.com>
2026-02-15 00:57:09 +0700

    Update checkin.go

--- models/checkin.go
+++ models/checkin.go
@@ -5,15 +5,22 @@ import (
 )
 
 type Checkin struct {
-	ID             uint `gorm:"primaryKey"`
-	UserID         uint `gorm:"index"`
-	CheckinAt      time.Time
-	CheckoutAt     *time.Time
-	CheckinPhoto   []byte
-	CheckoutPhoto  []byte
-	IsAutoCheckin  bool    `gorm:"column:is_auto_checkin;default:false"`
-	IsAutoCheckout bool    `gorm:"column:is_auto_checkout;default:false"`
-	AutoReason     *string `gorm:"column:auto_reason"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	ID             uint       `gorm:"primaryKey"`
+	UserID         uint       `gorm:"index;column:user_id"`
+	CheckinAt      time.Time  `gorm:"column:checkin_at"`
+	CheckoutAt     *time.Time `gorm:"column:checkout_at"`
+	CheckinPhoto   []byte     `gorm:"column:checkin_photo;type:longblob"`
+	CheckoutPhoto  []byte     `gorm:"column:checkout_photo;type:longblob"`
+	IsAutoCheckout bool       `gorm:"column:is_auto_checkout;default:false"`
+	CreatedAt      time.Time  `gorm:"column:created_at"`
+	UpdatedAt      time.Time  `gorm:"column:updated_at"`
+}
+
+type CheckinWithUser struct {
+	UserID        uint       `json:"user_id"`
+	FirstName     string     `json:"first_name"`
+	CheckinAt     time.Time  `json:"checkin_at"`
+	CheckoutAt    *time.Time `json:"checkout_at"`
+	CheckinPhoto  []byte     `json:"checkin_photo"`
+	CheckoutPhoto []byte     `json:"checkout_photo"`
 }
